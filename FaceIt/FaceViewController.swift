77b7ac14da521f08e9412b751a8bd1be1a75a6ef
muoivpham <muoi@niftit.com>
2017-01-07 12:17:44 -0600

    first commit

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by muoivpham on 1/6/17.
+//  Copyright © 2017 muoivpham. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+   
+}
+
